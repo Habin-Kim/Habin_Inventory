7f187b53fc26ede0ba14b5f717ecd6defe5f0b83
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-06-09 21:01:41 +0900

    STEP5
    STEP5 구현 (content 크기가 조금 크게 잡히는 문제 있음 내일 수정할 예정)

--- Assets/Scripts/UIInventory.cs
+++ Assets/Scripts/UIInventory.cs
@@ -1,13 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class UIInventory : MonoBehaviour
 {
     [SerializeField] private GameObject inventoryPanel;
     [SerializeField] private Button backButton;
 
+    [Header("슬롯 세팅")]
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private Transform slotParent; // 스크롤 뷰 컨텐트
+    [SerializeField] private int maxSlotCount = 18; //슬롯 최대 생성 수
+
+    private List<UISlot> slots = new List<UISlot>();
+
     private void Start()
     {
         backButton.onClick.AddListener(() => UIManager.Instance.OpenMainMenu());
+        InitInventoryUI();
+        SetContentSize();
+    }
+
+    private void InitInventoryUI()
+    {
+        for (int i = 0; i < maxSlotCount; i++)
+        {
+            GameObject slotGO = Instantiate(slotPrefab, slotParent);
+            UISlot slot = slotGO.GetComponent<UISlot>();
+            slots.Add(slot);
+        }
+    }
+
+    private void SetContentSize()
+    {
+        GridLayoutGroup grid = slotParent.GetComponent<GridLayoutGroup>();
+        int rows = Mathf.CeilToInt(maxSlotCount / grid.constraintCount);
+
+        float contentHeight = grid.padding.top + rows * (grid.cellSize.y + grid.spacing.y);
+        RectTransform rt = slotParent.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, contentHeight);
     }
 }
